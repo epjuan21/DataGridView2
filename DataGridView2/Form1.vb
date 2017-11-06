6b4bebaeab74bd2ea3099f078fab97ae2ca6b9a5
Juan Ramirez <ep.juan@gmail.com>
2017-11-06 10:08:41 -0500

    Uso de SQLDataAdapter, y DataTable para Llenar un DataGridView

--- DataGridView2/Form1.vb
+++ DataGridView2/Form1.vb
@@ -1,3 +1,35 @@
-﻿Public Class Form1
+﻿Imports System.Data.SqlClient
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Creacion de la Cadena de Conexion
+        Dim builder As New SqlConnectionStringBuilder
+        builder("Data Source") = "SERVIDOR01\SQLEXPRESS"
+        builder("Integrated Security") = True
+        builder("Initial Catalog") = "AdventureWorks2012"
 
+        Using connection As New SqlConnection(builder.ConnectionString)
+
+            'Dim adapter As SqlDataAdapter = New SqlDataAdapter("Select BusinessEntityID, NationalIDNumber, LoginID,OrganizationLevel From HumanResources.Employee", connection)
+            ' adapter.SelectCommand.CommandType = CommandType.Text
+
+
+            ' Otra Alternativa Seria Crear Un SQl Command
+            Dim cmd As SqlCommand = New SqlCommand("Select BusinessEntityID, NationalIDNumber, LoginID,OrganizationLevel From HumanResources.Employee", connection)
+            cmd.CommandType = CommandType.Text
+            'Luego SQL Data Adapter Con Otra Sobrecarga
+            Dim adapter As SqlDataAdapter = New SqlDataAdapter(cmd)
+
+            ' Esta tabla se almacena en la Memoria Cache
+            Dim tabla As DataTable = New DataTable
+
+            ' El DataAdapter al Momento de Hace e Fill abre la conexion y la cierra
+            adapter.Fill(tabla)
+
+            DataGridView1.DataSource = tabla
+
+        End Using
+
+
+
+    End Sub
 End Class
